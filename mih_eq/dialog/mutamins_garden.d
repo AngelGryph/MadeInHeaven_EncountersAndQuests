3c7bc231fb5e1c516a44adf1489d227671e080bc
Angel <angel@pearlgates.net>
2019-03-04 16:02:45 +0100

    Basilisks Galore!

---
+++ mih_eq/dialog/mutamins_garden.d
@@ -0,0 +1,32 @@
+APPEND "kirian"
+
+IF ~~
+BEGIN whatever
+  SAY @5
+    COPY_TRANS "kirian" 2
+END
+
+IF ~~
+BEGIN behind_you
+  SAY @6
+  IF ~~
+    DO ~ClearAllActions()
+	StartCutSceneMode()
+	StartCutScene("mh#bgcut")~
+    EXIT
+END
+
+END	// APPEND "kirian"
+
+ADD_TRANS_TRIGGER "kirian" 2 ~False()~
+
+EXTEND_BOTTOM "kirian" 2
+  IF ~~
+    REPLY @7
+    GOTO whatever
+  IF ~~
+    REPLY @8
+    GOTO behind_you
+END
+
+
