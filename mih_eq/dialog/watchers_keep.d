900c9d467fa23fe5768a092900dfb473010755e3
Angel <angel@pearlgates.net>
2019-07-09 17:16:06 +0200

    Enhanced Watcher's Keep

---
+++ mih_eq/dialog/watchers_keep.d
@@ -0,0 +1,10 @@
+REPLACE_ACTION_TEXT "gortan1"
+  ~GiveItemCreate("Helm29",LastTalkedToBy,0,0,0)~
+  ~GiveItem("helm29", LastTalkedToBy(Myself)~
+
+
+REPLACE_ACTION_TEXT "gorbat1"
+  ~GiveItemCreate("Helm29",LastTalkedToBy,0,0,0)~
+  ~GiveItem("book05", LastTalkedToBy(Myself)~
+
+
