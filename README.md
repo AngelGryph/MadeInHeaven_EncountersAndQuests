d2d313325e8eaf26b73d9ec5342432500c18689c
Angel <angel@pearlgates.net>
2019-12-02 11:59:40 +0100

    Final bug fixes & tweaks for v3

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ A quest pack for Baldur's Gate 1 and Baldur's Gate 2.
 
 ## Version History
 
-Version 3 - 13 June 2019
-- Added some BG2 stuff
+Version 3 - 2 December 2019
+- Added some SoD and BG2 stuff
 - Fixed many bugs
 
 Version 2 - 31 May 2019
--- mih_eq/dialog/watchers_keep.d
+++ mih_eq/dialog/watchers_keep.d
@@ -1,10 +1,10 @@
 REPLACE_ACTION_TEXT "gortan1"
   ~GiveItemCreate("Helm29",LastTalkedToBy,0,0,0)~
-  ~GiveItem("helm29", LastTalkedToBy(Myself)~
+  ~GiveItem("helm29", LastTalkedToBy(Myself))~
 
 
 REPLACE_ACTION_TEXT "gorbat1"
   ~GiveItemCreate("Helm29",LastTalkedToBy,0,0,0)~
-  ~GiveItem("book05", LastTalkedToBy(Myself)~
+  ~GiveItem("book05", LastTalkedToBy(Myself))~
 
 
