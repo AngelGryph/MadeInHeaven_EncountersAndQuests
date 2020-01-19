b07dbf936af7c5eb556a1953189dd87f614710f4
Angel <angel@pearlgates.net>
2020-01-19 16:54:49 +0100

    Converted Candlekeep Catacombs to SFO

---
+++ mih_eq/candlekeep_catacombs/compile_this/dopplegangers.d
@@ -0,0 +1,33 @@
+BEGIN "mh#dopkp"
+
+IF ~True()~
+BEGIN 0
+  SAY @7
+  IF ~~
+    DO ~ApplySpell(Myself, DOPPLEGANGER_CHANGE_DEFAULT_LESSER)~
+    EXIT
+END
+
+
+BEGIN "mh#doppr"
+
+IF ~True()~
+BEGIN 0
+  SAY @8
+  IF ~~
+    DO ~ApplySpell(Myself, DOPPLEGANGER_CHANGE_DEFAULT_LESSER)~
+    EXIT
+END
+
+
+BEGIN "erik3"
+
+IF ~True()~
+BEGIN 0
+  SAY @2
+  IF ~~
+    DO ~ApplySpell(Myself, DOPPLEGANGER_CHANGE_DEFAULT_LESSER)~
+    EXIT
+END
+
+
